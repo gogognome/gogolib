ec60cac3635f35c62216b5076342a5c0ba88ed4d
sanderk <sanderk>
2011-05-20 14:12:03 +0000

    Removed unnecessary check.

--- src/nl/gogognome/util/ComparatorUtil.java
+++ src/nl/gogognome/util/ComparatorUtil.java
@@ -1,5 +1,5 @@
 /*
- * $Id: ComparatorUtil.java,v 1.2 2009-01-03 12:20:58 sanderk Exp $
+ * $Id: ComparatorUtil.java,v 1.3 2011-05-20 14:12:03 sanderk Exp $
  *
  * Copyright (C) 2006 Sander Kooijmans
  */
@@ -15,7 +15,7 @@ public class ComparatorUtil {
     /**
      * Checks whether <code>o1</code> and <code>o2</code> are equal.
      * If both are <code>null</code>, then they are also considered equal.
-     * 
+     *
      * @param o1 an object (may be <code>null</code>)
      * @param o2 another object (possibly the same object) (may be <code>null</code>)
      * @return <code>true</code> if <code>o1</code> and <code>o2</code> are equal;
@@ -25,15 +25,11 @@ public class ComparatorUtil {
         if (o1 == o2) {
             return true;
         }
-        
-        if (o1 == null && o2 == null) {
-            return true;
-        }
-        
+
         if (o1 == null || o2 == null) {
             return false;
         }
-        
+
         return o1.equals(o2);
     }
 
@@ -42,7 +38,7 @@ public class ComparatorUtil {
      * equal to or larger than <code>n</code>.
      * @param m an int
      * @param n an int
-     * @return -1, 0 or 1 if <code>m</code> is less than, equal to or larger 
+     * @return -1, 0 or 1 if <code>m</code> is less than, equal to or larger
      *         than <code>n</code>
      */
     public static int compareTo(int m, int n) {
