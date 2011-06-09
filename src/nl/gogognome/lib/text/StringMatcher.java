5dc84bc27c190000256e145f1f67864277787a85
sanderk <sanderk>
2011-06-09 09:38:56 +0000

    Changed license to Apache License, Version 2.0.

--- src/nl/gogognome/lib/text/StringMatcher.java
+++ src/nl/gogognome/lib/text/StringMatcher.java
@@ -1,24 +1,27 @@
 /*
-    This file is part of gogolib.
+   Copyright 2011 Sander Kooijmans
 
-    gogolib is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
 
-    gogolib is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+     http://www.apache.org/licenses/LICENSE-2.0
 
-    You should have received a copy of the GNU General Public License
-    along with gogolib.  If not, see <http://www.gnu.org/licenses/>.
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
 */
 package nl.gogognome.lib.text;
 
 /**
- * @author sanderk
+ * This class implements a fast string matching algorithm (the Boyer-Moore string search algorithm).
+ * A StringMatcher instance contains a string which is considered a pattern.
+ * The StringMacther can find instances of this pattern within strings very efficiently.
  *
+ * <p>Use this class when you have to find a string in many other strings.
+ * @author Sander Kooijmans
  */
 public class StringMatcher {
 
