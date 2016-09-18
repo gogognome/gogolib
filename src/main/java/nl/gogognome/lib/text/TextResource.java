f468b57958e6a21557ed7a6a1f140071e2704f3e
Sander Kooijmans <sander@gogognome.nl>
2016-09-18 13:19:42 +0200

    Don't log when text resource is loaded. Removed some comments.

--- src/main/java/nl/gogognome/lib/text/TextResource.java
+++ src/main/java/nl/gogognome/lib/text/TextResource.java
@@ -1,18 +1,3 @@
-/*
-   Copyright 2011 Sander Kooijmans
-
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-     http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-*/
 package nl.gogognome.lib.text;
 
 import java.text.DateFormat;
@@ -37,8 +22,6 @@ import org.slf4j.LoggerFactory;
  * This class represents a text resource. It offers functionality to
  * obtain texts from resource files. It also offers easy methods to
  * format strings.
- *
- * @author Sander Kooijmans
  */
 public class TextResource {
 	private final Locale locale;
@@ -77,7 +60,6 @@ public class TextResource {
     public void loadResourceBundle(String resourceBundle) {
         ResourceBundle b = ResourceBundle.getBundle(resourceBundle, locale);
         stringResources.add(0, b);
-        LOGGER.debug("Loaded resource bundle " + resourceBundle + " for locale " + locale);
     }
 
 	/**
